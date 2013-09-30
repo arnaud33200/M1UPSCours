0e10e125889c48dbd6068f23519685bd37415ecb
arnaud33200 <arnaud33200@gmail.com>
2013-09-30 23:15:44 +0200

    TRP - TP1 : suite, exo 3 fini debut exo 4

--- TRP/TP1/TP1_TRP.ml
+++ TRP/TP1/TP1_TRP.ml
@@ -1,6 +1,13 @@
+(Printf.printf " ");;
+(Printf.printf "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");;
+(Printf.printf " ~~~~~~~~~ IMPORT GRAPHES ");;
+(Printf.printf "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");;
+(Printf.printf " ");;
 #use "graphes.ml";;
 
-(* ########### EXERCICE I ############ *)
+(Printf.printf " ");;
+(Printf.printf "######### EXERCICE I ######## ");;
+(Printf.printf " ");;
 
 let rec deepSearch p =
 match p with 
@@ -27,7 +34,9 @@ else (deepSearch ((etatsSuivants1 x)@r));;
 (* (deepSearch ["A"]);;    (* Exception: Failure "pas de solution". *) *)
 
 
-(* ########### EXERCICE II ############ *)
+(Printf.printf " ");;
+(Printf.printf "######### EXERCICE II ########");;
+(Printf.printf " ");;
 
 let addUnique e l =
 if (List.mem e l) then e::l else l;;
@@ -48,25 +57,39 @@ else (if (not (List.mem x v)) then (deepSearchV2 ((etatsSuivants2 x)@p) (x::v))
 (deepSearchV2 ["A"] []);;
 
 
-(* ########### EXERCICE III ############ *)
 (Printf.printf " ");;
 (Printf.printf "########### EXERCICE III ############");;
 (Printf.printf " ");;
 
-let rec insertLast e l = 
-(
- match l with
-       []->[e]
-       |x::r->x::(insertLast e r)
+let rec insertLast e l = (
+			  match l with
+				[]->[e]
+				|x::r->x::(insertLast e r)
 );;
 
-let rec deepSearchV3 p v =
-(
- match p with 
-       []-> failwith "pas de solution"
-       |x::r-> if (estBut2 x) then v
-else (if (not (List.mem x v)) then (deepSearchV2 ((etatsSuivants3 x)@p) (insertLast x v))
-	 else (deepSearchV3(diff p [x]) v))
+let rec deepSearchV3 lc v = ( 
+match lc with 
+       [] -> failwith "aucune solution"
+| c::rc -> ( 
+	    match c with
+		  [] -> [];
+		  | x::r -> if (estBut2 x) then [x]
+else (if (not (List.mem x v)) 
+then (insertLast x (deepSearchV3 (((etatsSuivants2 x)@r)::rc) (insertLast x v)))
+	 else (deepSearchV3 (r::rc) v)) )
 );;
 
-(deepSearchV3 ["A"] []);;
+(deepSearchV3 [["A"]] []);;
+
+
+(Printf.printf " ");;
+(Printf.printf "########### EXERCICE IV ############");;
+(Printf.printf " ");;
+
+let rec getOperation info next = (
+			 match info with 
+			       [] -> failwith "informations vide"
+			       | (o,n,c)::r -> if n = next then o
+else (getOperation r next)
+);;
+
