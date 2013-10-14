69a4536d541cdd17c6957b2a6511f8b26bb941b1
arnaud33200 <arnaud33200@gmail.com>
2013-10-14 12:57:27 +0200

    TRP - TP1 : A* marche pour graphe 1 mais bizarrement pas très fonctionnel pour les autres graphes. code commenté

--- TRP/TP1/AEtoile.ml
+++ TRP/TP1/AEtoile.ml
@@ -1,6 +1,7 @@
 #use "graphes.ml";;
 (Printf.printf " ~~~~~~~~~ IMPORT GRAPHES \n");;
 
+(* insertion trié sur la valeur de l'element e dans la liste *)
 let rec insertTriCout e l = 
 (
 	match l with
@@ -12,21 +13,28 @@ let rec insertTriCout e l =
 			)		
 );;
 
+(* insertion d'un element à la fin de la liste *)
 let rec insertLast element theList = (
 	match theList with
 	| [] -> [element]
 	| x::r -> x::(insertLast element r)
 );;
 
+
 let estimChemin etatFils coutsFils valParcours = (
 	((hEtat1 etatFils) + coutsFils + valParcours)
 );;
 
+(* ajout les fils du noeud actuellement développé *)
+(* dans la file des noeuds à développer *)
+(* #### valParcours : valeur du chemin parcours par le noeud père *)
+(* #### cheminActuel : liste des opérations effectués sur les noeufs père *)
 let rec ajouterFils fils file valParcours cheminActuel =
 (
 	match fils with
 	| [] -> file
-	| (o,a,c)::r -> (insertTriCout (a,(estimChemin a c valParcours), (insertLast o cheminActuel)) (ajouterFils r file valParcours cheminActuel))
+	| (o,a,c)::r -> (insertTriCout (a,(estimChemin a c valParcours), (insertLast o cheminActuel)) 
+		(ajouterFils r file valParcours cheminActuel))
 );;
 
 
