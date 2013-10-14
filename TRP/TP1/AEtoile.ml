cc53e0dca0717e83deb455707acb82f59e1ffbd5
arnaud33200 <arnaud33200@gmail.com>
2013-10-14 12:44:45 +0200

    TRP - TP1 : A* terminer et fonctionnel

--- TRP/TP1/AEtoile.ml
+++ TRP/TP1/AEtoile.ml
@@ -5,33 +5,41 @@ let rec insertTriCout e l =
 (
 	match l with
 			| [] -> [e]
-			| (n,c)::r -> let (a,b)=e in 
+			| (etatfile,valeurfile,cheminfile)::r -> let (etatnouveau,valeurnouveau, cheminnouveau)=e in 
 			(
-				if b < c then (a,b)::(n,c)::r 
-				else (n,c)::(insertTriCout e r)
+				if valeurnouveau < valeurfile then (etatnouveau,valeurnouveau, cheminnouveau)::(etatfile,valeurfile,cheminfile)::r 
+				else (etatfile,valeurfile,cheminfile)::(insertTriCout e r)
 			)		
+);;
+
+let rec insertLast element theList = (
+	match theList with
+	| [] -> [element]
+	| x::r -> x::(insertLast element r)
 );;
 
 let estimChemin etatFils coutsFils valParcours = (
 	((hEtat1 etatFils) + coutsFils + valParcours)
 );;
 
-let rec ajouterFils fils file valParcours =
+let rec ajouterFils fils file valParcours cheminActuel =
 (
 	match fils with
 	| [] -> file
-	| (o,a,c)::r -> (insertTriCout (a,(estimChemin a c valParcours)) (ajouterFils r file valParcours))
+	| (o,a,c)::r -> (insertTriCout (a,(estimChemin a c valParcours), (insertLast o cheminActuel)) (ajouterFils r file valParcours cheminActuel))
 );;
 
-let rec profondeurA attent =
+
+(* ( Etat, coup, chemin) *)
+let rec profondeurA attent=
 (
 	match attent with
 	| [] -> failwith "etat non trouve"
-	| (etat,valeur)::r -> if (estBut1 etat) then (etat,valeur)::[]
-		else (etat,valeur)::(profondeurA (ajouterFils (opPoss1 etat) r (valeur - (hEtat1 etat))))
+	| (etat,valeur, chemin)::r -> if (estBut1 etat) then (chemin,valeur)
+		else (profondeurA (ajouterFils (opPoss1 etat) r (valeur - (hEtat1 etat)) chemin))
 );;
 
 let test = [("A",1);("B",2);("C",4);("D",7)];;
 
 #trace profondeurA;;
-profondeurA [("A",7)];;
+profondeurA [("A",7,[])];;
