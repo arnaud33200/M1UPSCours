5c863949b977e1d8a3be7ce36acbb3b056ab6021
arnaud33200 <arnaud33200@gmail.com>
2013-10-14 11:47:04 +0200

    MCPR - TP1 : TP terminé, extra : faire la denière version avec deux programmes a part lancé en simultané depuis le terminal

--- MCPR/TP1/exercice.c
+++ MCPR/TP1/exercice.c
@@ -4,53 +4,71 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-int * cpt;
+#define N 100000;
 
 int main(int argc, char const *argv[])
 {
 	// list de IPC
-
+	int n = N;
 // Générer une clé
 	key_t key = ftok("/deb/null",1);
 
 // créer une m
-	int semid = shmget( IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666 );
+	int semidcpt = shmget( IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666 );
+	int semidshare = shmget( IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666 );
 
-	printf("SEMID = %d\n", semid);
+	printf("SEMIDcpt = %d\n", semidcpt);
+	printf("SEMIShare = %d\n", semidshare);
 
-	cpt = (int *)shmat(semid, NULL,0);
+	int * init = shmat(semidcpt, NULL,0);
 
-	*cpt = 0;
-	*cpt++;
-	printf("INIT :: %d\n", *cpt );
+	*init = 0;
+	printf("INIT :: %d\n", *init );
 	int i;
-	int pid;
+	int pid1;
+	int pid2;
 
-	if( (pid = fork()) == 0 ) // fils
+	if( (pid1 = fork()) == 0 ) // fils
 	{
-		cpt = (int *)shmat(semid, NULL,0);
-		for (i = 0; i < 50; ++i)
+		printf("########### FILS 1 ###########\n");
+		int *cptSon = shmat(semidcpt, NULL,0);
+		for (i = 0; i < n; ++i)
 		{
-			*cpt++;
+			(*cptSon) += 1;
 		}
-		printf("	** Compteur Fils = %d\n", *cpt );
-		shmdt(cpt);
+			printf("  ** Compteur final Fils = %d\n", *cptSon );
+		shmdt(cptSon);
+		int * val = shmat(semidshare, NULL, 0);
+		(*val) = 15;
 		exit(0);
 	}
-	else
+	
+	if( (pid1 = fork()) == 0 ) // fils
 	{
-		cpt = (int *)shmat(semid, NULL,0);
-		for (i = 0; i < 50; ++i)
+		printf("########### FILS 2 ###########\n");
+		int * cptFather = shmat(semidcpt, NULL,0);
+		for (i = 0; i < n; ++i)
 		{
-			*cpt--;
+			(*cptFather) -= 1;
 		}
-			printf("~ Compteur Père = %d\n", *cpt );
-			shmdt(cpt);
+			printf("~ Compteur final Père = %d\n", *cptFather );
+		int * val = shmat(semidshare, NULL, 0);
+		printf("VAL = %d\n", *val );
+		shmdt(cptFather);
+		exit(0);
 	}
 
-	shmdt(cpt);
-	shmctl(semid, IPC_RMID, NULL);
+	wait(0);
+	wait(0);
+	printf("########### FIN ###########\n");
+	printf("FINAL :: %d\n", *init );
+
+	int * val = shmat(semidshare, NULL, 0);
+		printf("VAL = %d\n", *val );
+	shmdt(init);
+	shmctl(semidcpt, IPC_RMID, NULL);
 
 
 	return 0;
