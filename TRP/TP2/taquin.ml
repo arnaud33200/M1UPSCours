b232b7e9dd1efdb1909309220a16204a9a1cb076
arnaud33200 <arnaud33200@gmail.com>
2013-10-20 22:09:42 +0200

    TRP - TP2 : deplacement casevide terminé pour version avec operation autorisé dans état

--- TRP/TP2/taquin.ml
+++ TRP/TP2/taquin.ml
@@ -4,10 +4,10 @@
 type operation = Up | Left | Down | Right;;
 
 let vectorOfOperation = function
-       Up   -> (1,0)
-    |  Left -> (0,1)
-    |  Down   -> (-1,0)
-    |  Right  -> (0,-1)  ;;
+       Up   -> (0,-1)
+    |  Left -> (-1,0)
+    |  Down   -> (0,1)
+    |  Right  -> (1,0)  ;;
 
 let dim = 3;;
 
@@ -19,14 +19,28 @@ let (e1:etat) = ([["A";"C";"D"];
 ["H";"F";"B"];
 ["G";" ";"E"]], (1,2), [Up; Left; Right] );;
 
-let (b1:etat) = ([["A";"B";"C"];
+let (b1:etat) = 
+([["A";"B";"C"];
 ["H";" ";"D"];
 ["G";"F";"E"]],(1,1), [Up; Left; Down; Right] );;
 
-let rec allowedOperation operation listOperation = (
-	match listOperation with
-	| [] -> false
-	| o::r -> if operation = o then true else (allowedOperation operation listOperation)	
+(* Determine si une opération est autorisé *)
+let isAllowedOperation newdest = (
+	if (fst newdest) < 0 || (fst newdest) >= (dim-1) || 
+	(snd newdest) < 0 || (snd newdest) >= (dim-1) then false else true
+);;
+
+let setAllowedOperation	p = (
+	if (fst p) = 0 && (snd p) = 0 then [Down; Right]
+	else if (fst p) = 0 && (snd p) >= (dim-1) then [Up; Right]
+	else if (fst p) >= (dim-1) && (snd p) >= (dim-1) then [Up; Left]
+	else if (fst p) >= (dim-1) && (snd p) = 0 then [Down; Left]
+	else if (fst p) = 0 then [Down; Up; Right]
+	else if (fst p) >= (dim-1) then [Down; Up; Left]
+	else if (snd p) = 0 then [Left; Up; Right]
+	else if (snd p) >= (dim-1) then [Left; Down; Right]
+	else [Up; Left; Down; Right]
+	
 );;
 
 (* récupère un element x y de la matrice *)
@@ -34,37 +48,53 @@ let rec allowedOperation operation listOperation = (
 	(* - si x = 0 alors on est sur la bonne colonne et on retourne l'element *)
 let rec getElementMatrice x y matrice = (
 	match matrice with
-	| [] -> failwith "element not found"
+	| [] -> "X"
 	| line::otherLine -> if (y != 0) then (getElementMatrice x (y - 1) otherLine)
 		else (
 			match line with
-			| [] -> failwith "element not found"
+			| [] -> "X"
 			| column::otherColumn -> if (x != 0) 
 					then (getElementMatrice (x-1) y (otherColumn::otherLine))
 				else column
 		)
 );;
 
-(* Echange la case originPos avec la case destPos dans la matrice *)
-let rec switchPosition originPos destPos matrice = (
-	let (x0,y0) = originPos and (x1,x2) = destPos in (
-		match matrice with
+(* Pour chaque ligne de la matrice, remplace l'element à changé à la position destPos *)
+	(* et fait un remplacement par la position trou à la position originPos *)
+let rec treatLines originPos destPos line x y matrice = (
+	let (x0,y0)=originPos and (x1,y1)=destPos in (
+		match line with
 		| [] -> []
-		| line::otherLine -> if x0 != 
+		| element::rest -> if (x,y) = originPos then (getElementMatrice x1 y1 matrice)::(treatLines originPos destPos rest (x+1) y matrice)
+			else (if (x,y) = destPos then (getElementMatrice x0 y0 matrice)::(treatLines originPos destPos rest (x+1) y matrice)
+				else element::(treatLines originPos destPos rest (x+1) y matrice)
+			)
 	)
+);;
+
+(* Echange la case originPos avec la case destPos dans la matrice *)
+let rec switchPosition originPos destPos x y lines matrice = (
+	match lines with
+	| [] -> []
+	| line::otherLine -> (treatLines originPos destPos line 0 y matrice)::(switchPosition originPos destPos 0 (y+1) otherLine matrice)
 );;
 
 let rec deplacer (etat:etat) (operation:operation) = (
 	let (taquin, posVide, allowedOp)=etat in (
-		if (not (allowedOperation operation allowedOp)) then
-			(failwith "Operation non autorisé")
-		else (
-			(switchPosition posVide 
-				((fst posVide)+(fst (vectorOfOperation operation)),
-					(snd posVide)+(snd (vectorOfOperation operation)))
-				taquin)
+		let destPos=((fst posVide)+(fst (vectorOfOperation operation)),
+					(snd posVide)+(snd (vectorOfOperation operation))) in (
+			if (not (isAllowedOperation destPos)) then failwith "Operation non autorise"
+			else ((switchPosition posVide destPos 0 0 taquin taquin), destPos, setAllowedOperation destPos)
 		)
 	)
 );;
 
-e1;;
+#trace deplacer;;
+(* #trace switchPosition;; *)
+(* #trace treatLines;; *)
+(* #trace getElementMatrice;; *)
+(* (deplacer e1 Down);; *)
+(deplacer b1 Up);;
+(deplacer b1 Down);;
+
+(* (treatLines (1,0) (2,0) ["A";"B";"C"] 0 0 [["A";"B";"C"];["H";" ";"D"];["G";"F";"E"]]);; *)
