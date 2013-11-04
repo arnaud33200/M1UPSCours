e9a7d932799d8ebda8f78e0ef3325559629f1e62
arnaud33200 <arnaud33200@gmail.com>
2013-11-04 09:05:21 +0000

    MCPR - TP2 : terminé

--- MCPR/TP2/first.c
+++ MCPR/TP2/first.c
@@ -19,7 +19,7 @@ int main(int argc, char const *argv[])
 	
 	int n = N;
 	int i;
-	int nbFils = 20;
+	int nbFils = 30;
 	int pidFils[nbFils];
 
 	// list de IPC
@@ -27,43 +27,10 @@ int main(int argc, char const *argv[])
 // ~~~~~~~ INIT SEMAPHORE ~~~~~~~~~
 	// variable partager pour le sémaphore
 	int semid = semget(ftok("./key",1), nbFils, IPC_CREAT | 0666);
-	for (i = 0; i < nbFils; ++i)
-	{
-		semctl(semid, i, SETVAL, 0);
-	}
 	// Operation des sémaphore : P & V
 	initSembuf(nbFils);
-	// struct sembuf p; p.sem_num = 0; p.sem_op = -1; p.sem_flg = 0;
-	// struct sembuf v; v.sem_num = 0; v.sem_op = 1; v.sem_flg = 0;
-
-// ~~~~~~~~~ LANCEMENT DES NBFILS PAR LE PÈRE ~~~~~~~~~~
-	
-	for (i = 0; i < nbFils; ++i)
-	{
-		if( (pidFils[i] = fork()) == 0 ) // fils
-		{
-			int nb = i+1;
-			int j;
-			for (j = 0; j < n; ++j)
-			{
-				semop(semid, &p[i], 1);
-				printf("Fils %d Message\n", nb);
-				semop(semid, &v[(i+1)%nbFils], 1);
-			}
-			exit(0);
-		}
-	}
 
 	semop(semid, &v[0], 1);
-	for (i = 0; i < nbFils; ++i)
-	{
-		wait(0);
-	}
-	
-	printf("########### FIN ###########\n");
-
-	semctl(semid, 0, IPC_RMID, 0);
-	eraseSembuf();
 	return 0;
 }
 
--- MCPR/TP2/ind.c
+++ MCPR/TP2/ind.c
@@ -16,10 +16,10 @@ void eraseSembuf();
 
 int main(int argc, char const *argv[])
 {
-	
+	int nb = atoi(argv[1]);
 	int n = N;
 	int i;
-	int nbFils = 20;
+	int nbFils = 30;
 	int pidFils[nbFils];
 
 	// list de IPC
@@ -27,43 +27,25 @@ int main(int argc, char const *argv[])
 // ~~~~~~~ INIT SEMAPHORE ~~~~~~~~~
 	// variable partager pour le sémaphore
 	int semid = semget(ftok("./key",1), nbFils, IPC_CREAT | 0666);
-	for (i = 0; i < nbFils; ++i)
-	{
-		semctl(semid, i, SETVAL, 0);
-	}
+	printf("fils %d get semid %d\n", nb, semid);
 	// Operation des sémaphore : P & V
 	initSembuf(nbFils);
-	// struct sembuf p; p.sem_num = 0; p.sem_op = -1; p.sem_flg = 0;
-	// struct sembuf v; v.sem_num = 0; v.sem_op = 1; v.sem_flg = 0;
+
 
 // ~~~~~~~~~ LANCEMENT DES NBFILS PAR LE PÈRE ~~~~~~~~~~
-	
-	for (i = 0; i < nbFils; ++i)
+
+	for (i = 0; i < n; ++i)
 	{
-		if( (pidFils[i] = fork()) == 0 ) // fils
-		{
-			int nb = i+1;
-			int j;
-			for (j = 0; j < n; ++j)
-			{
-				semop(semid, &p[i], 1);
-				printf("Fils %d Message\n", nb);
-				semop(semid, &v[(i+1)%nbFils], 1);
-			}
-			exit(0);
-		}
+		semop(semid, &p[nb], 1);
+		printf("Fils %d Message\n", nb);
+		semop(semid, &v[(nb+1)%nbFils], 1);
 	}
 
-	semop(semid, &v[0], 1);
-	for (i = 0; i < nbFils; ++i)
-	{
-		wait(0);
-	}
-	
 	printf("########### FIN ###########\n");
 
-	semctl(semid, 0, IPC_RMID, 0);
 	eraseSembuf();
+	if( nb == nbFils-1)
+		semctl(semid, 0, IPC_RMID, 0);
 	return 0;
 }
 
