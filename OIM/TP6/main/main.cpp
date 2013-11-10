3ac9acc70f36fa25552c1e56ccece1e8708e23c1
arnaud33200 <arnaud33200@gmail.com>
2013-11-10 14:57:39 +0000

    OIM - TP6 : compilation fonctionnel, execution fonctionnel à l'exeption pour la reflection et refraction en meme temps qui fait une erreur de seg

--- OIM/TP6/main/main.cpp
+++ OIM/TP6/main/main.cpp
@@ -11,26 +11,29 @@ Color compute_direct_lighting (Ray ray_, Isect isect_)
 /**
 * \todo : compute here direct lighting
 *
-* Le rôle de la fonction compute_direct_lighting() est de calculer la somme, pour chaque source de lumière de la scène, de l'éclairage
-* élémentaire dû à cette source. Cette somme est calculée en ajoutant à une couleur noire (triplet \b RGB [0, 0, 0], élément neutre de 
-* l'addition des couleurs), la valeur de l'éclairage direct lié à chaque source de la scène. La fonction compute_direct_lighting()
-* utilise pour cela les fonctions des modules \ref SceneAPI (pour accéder à l'ensemble des sources lumineuses de la scène), 
-* \ref RayAPI (pour calculer la visibilité entre un point et une source de lumière) et \ref LightAPI (pour calculer l'éclairage direct).
-*/
-Color c;
-c = init_color(0.f,0.f,0.f);
-for (int i = 0; i < nb_lights(); ++i)
-{
-	Light * l = get_light(i);
-	
-	if ( test_visibility (isect_, l) )
+* Le rôle de la fonction compute_direct_lighting() est de calculer la somme, pour chaque 
+* source de lumière de la scène, de l'éclairage élémentaire dû à cette source. Cette somme 
+* est calculée en ajoutant à une couleur noire (triplet \b RGB [0, 0, 0], élément neutre de 
+* l'addition des couleurs), la valeur de l'éclairage direct lié à chaque source de la scène. 
+* La fonction compute_direct_lighting()utilise pour cela les fonctions des modules \ref 
+* SceneAPI (pour accéder à l'ensemble des sources lumineuses de la scène), \ref RayAPI (pour 
+* calculer la visibilité entre un point et une source de lumière) et \ref LightAPI (pour 
+* calculer l'éclairage direct).
+* */
+	Color c;
+	c = init_color(0.f,0.f,0.f);
+	for (int i = 0; i < nb_lights(); ++i)
 	{
-
+		Light * l = get_light(i);
+		
+		if ( test_visibility (isect_, l) )
+		{
+			Color lc = direct_lighting(ray_, isect_, l);
+			c = add_color_color(c, lc);
+		// std::cout << "debut de get lights" << std::endl;
+		}
 	}
-}
-
-
-return c;
+	return c;
 
 }
 
@@ -41,17 +44,34 @@ Fonction à écrire par les etudiants
 Color trace_ray (Ray ray_)
 {
 	Color c = init_color (1.f, 1.f, 1.f);
-	Isect * i;
-	if ( intersect_scene (&ray_, i) == 0 )
+	Isect ise;
+	if ( intersect_scene (&ray_, &ise) == 0 )
 		return init_color (1.f, 1.f, 1.f);
-	c = add_color_color(c, compute_direct_lighting(ray_, *i));
+	c = add_color_color(c, compute_direct_lighting(ray_, ise));
 	if ( c.is_zero() )
 		return c;
-	if ( i->bsdf().has_reflection() )
-	Ray * ListRay;
-	// utiliser la reflexion pour généré d'aute ray
-	// utiliser la refraction
+	// if ( ise.bsdf().has_reflection() )
+	
 
+	if ( isect_has_refraction(ise) && ! c.is_zero() )
+	{
+		Ray refractRay;
+		Color rf = refract(ray_, ise, &refractRay);
+		c = multiply_color_color(c, rf);
+		// std::cout << "refract" << std::endl;
+		c = add_color_color(c, trace_ray(refractRay));
+	}
+
+	// if ( isect_has_reflection(ise) && ! c.is_zero() )
+	// {
+	// 	Ray reflectRay;
+	// 	Color r = reflect(ray_, ise, &reflectRay);
+	// 	c = multiply_color_color(c, r);
+	// 	c = add_color_color(c, trace_ray(reflectRay));
+	// }
+
+
+	// utiliser la refraction
 
 
 /**
@@ -78,10 +98,12 @@ Color trace_ray (Ray ray_)
 * calculée en lançant le rayon réfléchi devra alors être multipliée par ce coefficient avant d'être ajoutée
 * à la couleur renvoyée par trace_ray().
 *
-* Pour la troisème étape et de façon très similaire à la réflexion, utiliser les fonctions définies dans le module \ref LightAPI pour calculer la couleur réfractée.
-* Pour cela, il  faut tester si le matériau est transparent et, si c'est le cas, calculer le rayon réfracté et le coefficient de 
-* transparence (une couleur). La couleur calculée en lançant le rayon réfracté devra alors être multipliée par ce coefficient avant
-* d'être ajoutée à la couleur renvoyée par trace_ray().
+* Pour la troisème étape et de façon très similaire à la réflexion, utiliser les fonctions 
+* définies dans le module \ref LightAPI pour calculer la couleur réfractée. Pour cela, il 
+* faut tester si le matériau est transparent et, si c'est le cas, calculer le rayon réfracté 
+* et le coefficient de  transparence (une couleur). La couleur calculée en lançant le rayon 
+* réfracté devra alors être multipliée par ce coefficient avant d'être ajoutée à la couleur 
+* renvoyée par trace_ray().
 *
 */
 
@@ -131,12 +153,13 @@ int main () {
 
 //make_platon_scene (800, 600);
 
-	std::cout << "BONJOUR" << std::endl;
 	make_default_scene(800, 600);
 
+	std::cout << "BONJOUR" << std::endl;
 	compute_image();
 
-	save_image ("monimage.exr");
+	char * path = "monimage.exr";
+	save_image ((char*)path);
 
 	return 0;
 }
