8a6c2ced332002725fad62d098d49b6e4de1a47f
arnaud33200 <arnaud33200@gmail.com>
2013-11-24 20:12:26 +0000

    MCPR - TP4 : java ne reconnais pas Condition T__T

--- MCPR/TP4/MonitorHoar.java
+++ MCPR/TP4/MonitorHoar.java
@@ -1,20 +1,41 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 public class MonitorHoar
 {
-	Lock 
+	private int auTourDe;
+	private int nbUtilisateur;
+	final private Condition attenteTour0;
+	final private Condition attenteTour1;
 
 	public MonitorHoar()
 	{
-
+		auTourDe = 0;
+		nbUtilisateur = 0;
+		attenteTour0 = new AbstractQueuedSynchronizer.ConditionObject();
+		attenteTour1 = new AbstractQueuedSynchronizer.ConditionObject();
 	}
 
-	public void accederTable()
+	public void accederTable(int num)
 	{
+		lock.lock();
+
+		if( nbUtilisateur > 0 || auTourDe != num)
+		{
+			if( num == 0) attenteTour0.await();
+			else attenteTour1.await();
+		}
+		else
+			nbUtilisateur = 1;
 
 	}
 
 	public void libererTable()
 	{
-
+		auTourDe = (auTourDe +1 )%2;
+		nbUtilisateur = 0;
+		if( num == 0) attenteTour0.signal();
+		else attenteTour1.signal();
 	}
 }
