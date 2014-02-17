f64e8a7bc60ea524b948401dc5d736f5905cc375
unknown <21309581@204-04.mig.snap.fsi>
2014-02-17 11:49:18 +0100

    DCLL - TP2 : debut exo2 ajout de l'interface

---
+++ DCLL/TP2/MySimpleStack/src/main/java/dcll/alra/mysimplestak/SimpleStack.java
@@ -0,0 +1,10 @@
+import java.lang.*
+import java.util.EmptyStackException;
+
+public interface SimpleStack {
+	public boolean isEmpty();
+	public int getSize();
+	public void push(Item item);
+	public Item peek() throws EmptyStackException;
+	public Item pop() throws EmptyStackException;
+}
